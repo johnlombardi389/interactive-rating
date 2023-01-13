6cef14cd198dc48dbbda7cd923195f994a560ccd
johnlombardi389 <lombardij38@gmail.com>
2023-01-13 15:04:53 -0500

    Added base p font styling

--- src/components/GetRating.jsx
+++ src/components/GetRating.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 function GetRating() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <p>Hello World</p>
     </div>
   );
 }
--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -6,6 +6,12 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     box-sizing: border-box;
   }
+
+  p {
+    font-family: 'Overpass', sans-serif;
+    font-weight: 400;
+    font-size: 0.95rem;
+  }
 `;
 
 export default GlobalStyles;

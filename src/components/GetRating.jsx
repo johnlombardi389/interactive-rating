e24c6c14e7768b34230561a35acf625ea5bc79cf
johnlombardi389 <lombardij38@gmail.com>
2023-01-17 16:01:01 -0500

    Added h1, p, and button to card

--- src/components/GetRating.jsx
+++ src/components/GetRating.jsx
@@ -5,7 +5,14 @@ function GetRating() {
   return (
     <div>
       <Card>
-        <p>Hello</p>
+        {/* <Star></Star> */}
+        <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        {/* <Choices></Choices> */}
+        <button>Submit</button>
       </Card>
     </div>
   );
@@ -16,7 +23,17 @@ const Card = styled.div`
   max-height: 500px;
   background-color: hsl(213, 19%, 18%);
   border-radius: 2.5rem;
-  padding: 5rem;
+  padding: 2rem;
+  h1 {
+    margin: 2rem 0;
+  }
+  p {
+    margin-bottom: 2rem;
+  }
+  button {
+    margin-top: 2rem;
+    padding: 1rem;
+  }
 `;
 
 export default GetRating;

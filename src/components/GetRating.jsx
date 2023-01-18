6609d909ca15d1d1129f0db7ae90785f7a7a35b7
johnlombardi389 <lombardij38@gmail.com>
2023-01-18 15:24:45 -0500

    Created function to handle rating clicks and set rating state as well as change background color

--- src/components/GetRating.jsx
+++ src/components/GetRating.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 // Style
 import styled from "styled-components";
 // Icon
 import StarIcon from "../assets/images/icon-star.svg";
 
 function GetRating() {
+  const [rating, setRating] = useState(0);
+
+  const ratingHandler = (e) => {
+    setRating(parseInt(e.target.innerHTML));
+
+    const numbers = e.target.parentElement.children;
+
+    for (let i = 0; i < numbers.length; i++) {
+      numbers[i].classList.add("rating-default");
+      numbers[i].classList.remove("rating-active");
+    }
+    e.target.classList.add("rating-active");
+    e.target.classList.remove("rating-default");
+  };
+
   return (
     <div>
       <Card>
@@ -13,17 +29,27 @@ function GetRating() {
             alt="A star icon showing this card is about giving a rating"
           />
         </Star>
-        <h1>How did we do?</h1>
+        <h1>How did we do? {rating}</h1>
         <p>
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
         <Choices>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
+          <li onClick={ratingHandler} className="rating-default">
+            1
+          </li>
+          <li onClick={ratingHandler} className="rating-default">
+            2
+          </li>
+          <li onClick={ratingHandler} className="rating-default">
+            3
+          </li>
+          <li onClick={ratingHandler} className="rating-default">
+            4
+          </li>
+          <li onClick={ratingHandler} className="rating-default">
+            5
+          </li>
         </Choices>
         <button>Submit</button>
       </Card>
@@ -85,8 +111,23 @@ const Choices = styled.ul`
   justify-content: space-between;
   align-items: center;
   list-style: none;
-  li {
+  .rating-active {
     color: white;
+    width: 1rem;
+    height: 1rem;
+    padding: 1.75rem;
+    background: #fc7613;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    font-size: 1rem;
+    font-family: "Overpass", sans-serif;
+    font-weight: 400;
+  }
+  .rating-default {
+    color: #717882;
     width: 1rem;
     height: 1rem;
     padding: 1.75rem;
@@ -99,6 +140,11 @@ const Choices = styled.ul`
     font-size: 1rem;
     font-family: "Overpass", sans-serif;
     font-weight: 400;
+    transition: all 0.3s ease-in-out;
+    &:hover {
+      background: #7f8b9b;
+      color: white;
+    }
   }
 `;
 
